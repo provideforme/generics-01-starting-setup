12c8c4646965e68d564a7ca0684277204330b817
provideforme <evan.a.bernard@gmail.com>
2022-05-08 08:58:56 -0400

    generic classes

--- src/app.ts
+++ src/app.ts
@@ -45,3 +45,39 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({name: 'Max'}, 'name'));
+
+class DataStorage<T extends string | number | boolean > {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if(this.data.indexOf(item) === -1) {
+      return
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max')
+textStorage.addItem('Manu')
+textStorage.removeItem('Max')
+
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>()
+// const maxObj = {name: 'Max'}
+// objStorage.addItem(maxObj);
+// objStorage.addItem({name: 'Manu'});
+// // ...
+// objStorage.removeItem(maxObj)
+// console.log(objStorage.getItems());
