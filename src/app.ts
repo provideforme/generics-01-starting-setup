2a3217a8a9e741a2847a899ddeec7c0051f42e7f
provideforme <evan.a.bernard@gmail.com>
2022-05-08 08:25:32 -0400

    create generic type function

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
-const names: Array<string> = []; // same as string[]
+// const names: Array<string> = []; // same as string[]
 // names[0].split(' ');
 // better type saftey with generic type
 // Array knows what type of data it stores
 // Promise knows what type of data it returns
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(100)
-  }, 2000)
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(100)
+//   }, 2000)
+// });
 
-promise.then(data => {
+// promise.then(data => {
   // data.split(' ')
-})
+// })
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+//using generic types lets typescript know the data types can and often will be different
+
+const mergedObj = merge({name: 'Max'}, {age: 30});
+console.log(mergedObj.age);
