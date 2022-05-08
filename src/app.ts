b5d5610740dbcc3461d472d62ed591ace3b61650
provideforme <evan.a.bernard@gmail.com>
2022-05-08 08:40:07 -0400

    create generic function

--- src/app.ts
+++ src/app.ts
@@ -23,4 +23,21 @@ const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30} );
 
 console.log(mergedObj);
 
+interface Lengthy {
+  length: number
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if(element.length === 1){
+    descriptionText = 'Got 1 element';
+  } else if (element.length > 1){
+    descriptionText = 'Got ' + element.length + ' elements.'
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['Sports']));
+
+
 
