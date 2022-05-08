0fadca6f462b5b626968b36b193ee86880fa899b
provideforme <evan.a.bernard@gmail.com>
2022-05-08 08:18:06 -0400

    demonstrate generic promise

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 const names: Array<string> = []; // same as string[]
 // names[0].split(' ');
+// better type saftey with generic type
+// Array knows what type of data it stores
+// Promise knows what type of data it returns
 
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(100)
+  }, 2000)
+});
+
+promise.then(data => {
+  // data.split(' ')
+})
