145d6ddafd12ce7fbfb912d677a42848d3054792
provideforme <evan.a.bernard@gmail.com>
2022-05-08 08:32:55 -0400

    add constraints

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,13 @@
   // data.split(' ')
 // })
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 //using generic types lets typescript know the data types can and often will be different
 
-const mergedObj = merge({name: 'Max'}, {age: 30});
-console.log(mergedObj.age);
+const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30} );
+
+console.log(mergedObj);
+
+
